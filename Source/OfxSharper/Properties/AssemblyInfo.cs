06154dcc2bcd3875fc097c48c8c732bec02ee62f
Victor D. Sandiego <codepuppy@balticsoap.com>
2018-12-03 21:13:31 -0600

    Change the target framework to 4.5 and update assembly version to 3.1.1

--- Source/OfxSharper/Properties/AssemblyInfo.cs
+++ Source/OfxSharper/Properties/AssemblyInfo.cs
@@ -22,15 +22,5 @@ using System.Runtime.InteropServices;
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("79a39177-6cd1-4ebf-a6f6-523e34c67ecf")]
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.1.1.0")]
+[assembly: AssemblyFileVersion("3.1.1.0")]
